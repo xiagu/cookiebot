6ca6c5929a5d8e4fa690636699cb7d60511bdbaa
Andrew Rodgers-Schatz <arodgers@palantir.com>
2014-08-27 11:40:49 -0400

    Updated for latest version of Cookie Monster (doesn't support the beta version)

--- cookiestuff.js
+++ cookiestuff.js
@@ -31,48 +31,13 @@ var ID_LUCKY_DAY = 52,
 /* Called after Cookie Monster is loaded */
 function everything() { 
 
+	clearInterval(goldenCookieInterval);
+	clearTimeout(cheatyBuyTimeout);
+	clearTimeout(buyElderPledgeTimeout);
+	clearTimeout(autoClickTimeout);
+
 	/* Remove the alert box, since it requires human interaction to confirm and that will impede our automated cookie-making */
 	Game.Upgrades['One mind'].clickFunction = null;
-
-	/* Fix Get_True_CPI, the one included in Cookie Monster is both unused and broken  (as of Cookie Monster 1.35.04, approximately) */
-	Get_True_CPI = function(e, t) {
-	    var n = 0;
-	    var r = 0;
-	    var i = 0;
-	    if (t == "ob") {
-	        n = My_Seconds_Left(e, "ob");
-	        r = Game.ObjectsById[e].price;
-	        i = hold_is[e]
-	    }
-	    if (t == "up") {
-	        n = My_Seconds_Left(e, "up");
-	        r = Game.UpgradesById[e].basePrice;
-	        for (var s = 0; s < upgrade_count; s++) {
-	            if (_cup(s, e, true)) {
-		            if (!Game.UpgradesById[e].bought) {
-		                i = Manage_Tooltip(s, e, false, true);
-		                break;
-		            }
-		        }
-	        }
-	    }
-	    var o = r / i;
-	    Game.ObjectsById.forEach(function (s, u) {
-	        var a = s.price;
-	        var f = hold_is[u];
-	        var l = My_Seconds_Left(u, "ob");
-	        if (l < n && (t == "up" || u != e)) {
-	            var c = n - l;
-	            var h = f * c;
-	            var d = r - a + h;
-	            var v = d / i;
-	            if (v > o) {
-	                o = v
-	            }
-	        } else {}
-	    });
-	    return o;
-	}
 
 	My_Seconds_Left = function(id, t) {
 	  var n = 0;
@@ -161,9 +126,9 @@ function everything() {
 
 	cpsMeasure = function() {
 		var date = new Date();
-		avg = formatNum((Game.cookiesEarned - oldCE) * 1000 / (date.getTime() - start.getTime()));
+		avg = CM.Backup.Beautify((Game.cookiesEarned - oldCE) * 1000 / (date.getTime() - start.getTime()));
 		var realCps = gcbank(0)[0];
-		estimate = formatNum(realCps + myGoldenCookieCps(realCps,Game.cookies));
+		estimate = CM.Backup.Beautify(realCps + myGoldenCookieCps(realCps,Game.cookies));
 		console.log("Running average CPS since " + start.toLocaleTimeString() + " = " + avg + " \tEstimated CPS: " + estimate);
 		setTimeout(cpsMeasure, 10000);
 	}
@@ -175,18 +140,26 @@ function everything() {
 		savedPrice = Number.MAX_VALUE;
 		savedIncome = 0; 
 		savedTime = 0; // seconds until we can buy
+
+		// need for golden cookie upgrade calculations
+		var bank = gcbank(savedIncome);
+		var realCps = bank[0];
+		var reqBank = bank[1];
+		var realIncome = bank[2];
+
 		/* Check items to buy */
 		for(var i=0; i<Game.ObjectsN; i++) {
-			name = Game.ObjectsById[i].name;
-			value = Get_True_CPI(i, "ob");
-			income = hold_is[i];
-			if(value < min || (value == min && Game.ObjectsById[i].price < savedPrice)) {
+			var name = Game.ObjectsById[i].name;
+			var time = (Game.ObjectsById[i].price - Game.cookies) / realCps;
+			var value = CM.Cache.Objects[name].bci;
+			var income = CM.Cache.Objects[name].bonus;
+			if(value+time < min || (value+time == min && Game.ObjectsById[i].price < savedPrice)) {
+				min = value+time;
 				savedName = name;
-				min = value;
 				savedObject = Game.ObjectsById[i];
 				savedIncome = income;
 				savedPrice = savedObject.price;
-				savedTime = hold_tc[i];
+				savedTime = time;
 			}
 		}
 
@@ -200,9 +173,10 @@ function everything() {
 
 		/* Check available upgrades */
 		Game.UpgradesInStore.forEach(function (t, n) {
-			id = t.id;
-			name = t.name;
-			income = 0;
+			var id = t.id;
+			var name = t.name;
+			var income = 0;
+			var value = 0;
 
 			switch(name) {
 			/* Golden cookie upgrades, Elder Pledge and Sacrificial Rolling Pins */
@@ -238,21 +212,17 @@ function everything() {
 				break;
 			default:
 			   	// Instead of showing the tooltip and regexping it we call Cookie Monster's internal computation functions to get the income
-				value = Get_True_CPI(id, "up");
-				for (var s = 0; s < upgrade_count; s++) {
-	            	if (_cup(s, t.id, true)) {
-		                income = Manage_Tooltip(s, t.id, false, true);
-	                	break;
-            		}
-        		}
+				value = CM.Cache.Upgrades[name].bci;
+	        	income = CM.Cache.Upgrades[name].bonus;
 			}
-			if(value < min || (value == min && t.basePrice < savedPrice)) {
+			var time = (t.basePrice - Game.cookies) / realCps;
+			if(value+time < min || (value+time == min && t.basePrice < savedPrice)) {
 				savedName = name;
 				savedObject = t;
 				savedIncome = income;
-				min = value;
+				min = value+time;
 				savedPrice = t.basePrice;
-				savedTime = My_Seconds_Left(t.id, "up");
+				savedTime = time;
 			}
 	    });
 
@@ -278,7 +248,7 @@ function everything() {
 					waitTime = 0.25; // check faster to use up bank faster if we have surplus
 			else 
 				waitTime = Math.min(Math.max(savedTime / 2.0, 0.5), 300); // check at least once every 30 minutes
-			console.log( formatNum(myCookies - savedPrice) + " remaining, need more than " + formatNum(reqBank) + "\t will buy [" + savedObject.id + "] " + savedObject.name + ": " + willBuy + ";\tChecking again in " + formatTime(waitTime, null));
+			console.log( CM.Backup.Beautify(myCookies - savedPrice) + " remaining, need more than " + CM.Backup.Beautify(reqBank) + "\t will buy [" + savedObject.id + "] " + savedObject.name + ": " + willBuy + ";\tChecking again in " + CM.Disp.FormatTime(waitTime, null));
 			clearTimeout(cheatyBuyTimeout); // just in case we want to restart the timer, for instance, some big change happened
 			cheatyBuyTimeout = setTimeout(function() { cheatyBuy(); }, waitTime*1000);
 		}
@@ -310,7 +280,7 @@ function install() {
 	jA.onload = function() { 
 		var jB = document.createElement('script'); 
 		jB.setAttribute('type', 'text/javascript'); 
-		jB.setAttribute('src', 'http://pastebin.com/raw.php?i=2KRNm8Gm'); 
+		jB.setAttribute('src', 'http://aktanusa.github.io/CookieMonster/CookieMonster.js'); 
 		$(jB).load(everything);
 		document.body.appendChild(jB); 
 	}; 
